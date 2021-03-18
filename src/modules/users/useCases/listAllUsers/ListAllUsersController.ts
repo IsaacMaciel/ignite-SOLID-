0e52ce4ca39739039ba2aeb7aa34c2d883159718
Isaac Araujo <isaac_maciel@hotmail.com>
2021-03-17 21:37:01 -0300

    finalizando o desafio

---
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+
+import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
+
+class ListAllUsersController {
+  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+
+  handle(request: Request, response: Response): Response {
+    try {
+      const { user_id } = request.headers;
+      const id = user_id.toString()
+      const list = this.listAllUsersUseCase.execute({ user_id:id });
+  
+      return response.json(list) 
+      
+    } catch (error) {
+      return response.status(400).json({error});
+    }
+  }
+}
+
+export { ListAllUsersController };
